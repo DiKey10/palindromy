d7ced7b127358a1b5b7662332412125a9d49c449
DiKey10 <74766684+DiKey10@users.noreply.github.com>
2022-07-30 17:31:43 +0200

    with input

--- main.py
+++ main.py
@@ -6,3 +6,10 @@ Prosta funkcja ktora sprawdza czy dane słowo jest palidromem
 def sprawdzimy(slowo):
   print(bool(slowo == slowo[::-1]))
 sprawdzimy("100030001")
+
+
+
+
+print("A teraz prosze sam wymysl słowo a ja sprawczy czy jest palidromem")
+slowo = input()
+sprawdzimy(slowo)
